7ad023eb82ba927cca2c54b7aa5b168b347db28a
Kenford20 <kzhou0066@gmail.com>
2024-06-21 23:06:13 -0500

    refactor app logic into useCube hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,129 +1,30 @@
-import { useEffect, useState, useMemo } from "react";
 import "./App.css";
-import Cube from "cubejs/lib/cube";
-
-const defaultCubeState = [
-  [
-    [null, null, null, null, null, null, null, null, null],
-    ["U1", "U2", "U3", "U4", "U5", "U6", "U7", "U8", "U9"],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-  ],
-  [
-    ["L1", "L2", "L3", "L4", "L5", "L6", "L7", "L8", "L9"],
-    ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9"],
-    ["R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9"],
-    ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9"],
-  ],
-  [
-    [null, null, null, null, null, null, null, null, null],
-    ["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9"],
-    [null, null, null, null, null, null, null, null, null],
-    [null, null, null, null, null, null, null, null, null],
-  ],
-];
+import { useCube } from "./useCube";
 
 function App() {
-  const [cubeState, setCubeState] = useState(defaultCubeState ?? []);
-  const cube = useMemo(() => new Cube(), []);
-
-  useEffect(() => {
-    updateCube();
-  }, []);
-
-  const randomizeCube = () => {
-    cube.randomize();
-    updateCube();
-  };
-
-  // cubeAsString ex: UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB
-  const updateCube = () => {
-    const state = [...cubeState];
-    // process string by 9s aka a face on the cube
-    for (let i = 0; i <= 54; i += 9) {
-      const face = cube
-        .asString()
-        .slice(i, i + 9)
-        .split("")
-        .map((char, i) => `${char}${i + 1}`);
-
-      if (i === 0) state[0][1] = face; // top face
-      if (i === 9) state[1][2] = face; // right face
-      if (i === 18) state[1][1] = face; // front face
-      if (i === 27) state[2][1] = face; // down face
-      if (i === 36) state[1][0] = face; // left face
-      if (i === 45) state[1][3] = face; // back face
-    }
-    setCubeState(state);
-  };
-
-  // type direction = U | D | L | R | F | B | [direction]' = reverse
-  const rotateCube = (direction) => {
-    cube.move(direction);
-    updateCube();
-  };
+  const { cubeState, cubeStringified, rotateCube, randomizeCube } = useCube();
 
   return (
     <main id="app">
       <div style={{ display: "flex", alignItems: "center", gap: "20px" }}>
         <div>
           <h1>Front View</h1>
+          {/* pass in stringified cube state to query params of this url for image of cube */}
           <img
-            src={`https://cube.rider.biz/visualcube.php?fmt=svg&size=300&view=trans&fd=${cube
-              .asString()
-              .toLowerCase()}`}
+            src={`https://cube.rider.biz/visualcube.php?fmt=svg&size=300&view=trans&fd=${cubeStringified.toLowerCase()}`}
           />
         </div>
         <CubeActions rotateCube={rotateCube} />
         <div>
           <h1>Flipped View</h1>
           <img
-            src={`https://cube.rider.biz/visualcube.php?fmt=svg&size=300&view=trans&r=y45x-215&fd=${cube
-              .asString()
-              .toLowerCase()}`}
+            src={`https://cube.rider.biz/visualcube.php?fmt=svg&size=300&view=trans&r=y45x-215&fd=${cubeStringified.toLowerCase()}`}
           />
         </div>
       </div>
       <button onClick={randomizeCube}>Shuffle Cube</button>
       <CubeState cubeState={cubeState} />
     </main>
-  );
-}
-
-function CubeActions({ rotateCube }) {
-  return (
-    <div>
-      <div>
-        <button onClick={() => rotateCube("U'")}>{"<-"}</button>
-        <span>Rotate Top</span>
-        <button onClick={() => rotateCube("U")}>{"->"}</button>
-      </div>
-      <div>
-        <button onClick={() => rotateCube("D'")}>{"<-"}</button>
-        <span>Rotate Bottom</span>
-        <button onClick={() => rotateCube("D")}>{"->"}</button>
-      </div>
-      <div>
-        <button onClick={() => rotateCube("L'")}>{"<-"}</button>
-        <span>Rotate Left</span>
-        <button onClick={() => rotateCube("L")}>{"->"}</button>
-      </div>
-      <div>
-        <button onClick={() => rotateCube("R'")}>{"<-"}</button>
-        <span>Rotate Right</span>
-        <button onClick={() => rotateCube("R")}>{"->"}</button>
-      </div>
-      <div>
-        <button onClick={() => rotateCube("F'")}>{"<-"}</button>
-        <span>Rotate Front</span>
-        <button onClick={() => rotateCube("F")}>{"->"}</button>
-      </div>
-      <div>
-        <button onClick={() => rotateCube("B'")}>{"<-"}</button>
-        <span>Rotate Back</span>
-        <button onClick={() => rotateCube("B")}>{"->"}</button>
-      </div>
-    </div>
   );
 }
 
@@ -177,6 +78,43 @@ function CubeRow({ cubeFace, i }) {
         </span>
       ))}
     </section>
+  );
+}
+
+function CubeActions({ rotateCube }) {
+  return (
+    <div>
+      <div>
+        <button onClick={() => rotateCube("U'")}>{"<-"}</button>
+        <span>Rotate Top</span>
+        <button onClick={() => rotateCube("U")}>{"->"}</button>
+      </div>
+      <div>
+        <button onClick={() => rotateCube("D'")}>{"<-"}</button>
+        <span>Rotate Bottom</span>
+        <button onClick={() => rotateCube("D")}>{"->"}</button>
+      </div>
+      <div>
+        <button onClick={() => rotateCube("L'")}>{"<-"}</button>
+        <span>Rotate Left</span>
+        <button onClick={() => rotateCube("L")}>{"->"}</button>
+      </div>
+      <div>
+        <button onClick={() => rotateCube("R'")}>{"<-"}</button>
+        <span>Rotate Right</span>
+        <button onClick={() => rotateCube("R")}>{"->"}</button>
+      </div>
+      <div>
+        <button onClick={() => rotateCube("F'")}>{"<-"}</button>
+        <span>Rotate Front</span>
+        <button onClick={() => rotateCube("F")}>{"->"}</button>
+      </div>
+      <div>
+        <button onClick={() => rotateCube("B'")}>{"<-"}</button>
+        <span>Rotate Back</span>
+        <button onClick={() => rotateCube("B")}>{"->"}</button>
+      </div>
+    </div>
   );
 }
 
---
+++ src/useCube.js
@@ -0,0 +1,74 @@
+import { useEffect, useState, useMemo } from "react";
+import Cube from "cubejs/lib/cube"; // https://github.com/ldez/cubejs
+
+const defaultCubeState = [
+  [
+    [null, null, null, null, null, null, null, null, null],
+    ["U1", "U2", "U3", "U4", "U5", "U6", "U7", "U8", "U9"],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+  ],
+  [
+    ["L1", "L2", "L3", "L4", "L5", "L6", "L7", "L8", "L9"],
+    ["F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9"],
+    ["R1", "R2", "R3", "R4", "R5", "R6", "R7", "R8", "R9"],
+    ["B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9"],
+  ],
+  [
+    [null, null, null, null, null, null, null, null, null],
+    ["D1", "D2", "D3", "D4", "D5", "D6", "D7", "D8", "D9"],
+    [null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null],
+  ],
+];
+
+const useCube = () => {
+  const [cubeState, setCubeState] = useState(defaultCubeState ?? []);
+  const cube = useMemo(() => new Cube(), []);
+  // cubeStringified ex: UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB
+  const cubeStringified = cube.asString();
+
+  useEffect(() => {
+    updateCube();
+  }, []);
+
+  const randomizeCube = () => {
+    cube.randomize();
+    updateCube();
+  };
+
+  const updateCube = () => {
+    const updatedCube = [...cubeState];
+    // process 6 faces on the cube
+    for (let i = 0; i < 6; i++) {
+      const face = cubeStringified
+        .slice(i, i + 9) // each face has the 9 cells, represented as a substring of cubeStringified
+        .split("")
+        .map((char, i) => `${char}${i + 1}`);
+
+      if (i === 0) updatedCube[0][1] = face; // top face
+      if (i === 9) updatedCube[1][2] = face; // right face
+      if (i === 18) updatedCube[1][1] = face; // front face
+      if (i === 27) updatedCube[2][1] = face; // down face
+      if (i === 36) updatedCube[1][0] = face; // left face
+      if (i === 45) updatedCube[1][3] = face; // back face
+    }
+    setCubeState(updatedCube);
+  };
+
+  // type direction = U | D | L | R | F | B | [direction]' = reverse
+  const rotateCube = (direction) => {
+    cube.move(direction);
+    updateCube();
+  };
+
+  return {
+    cube,
+    cubeState,
+    cubeStringified,
+    rotateCube,
+    randomizeCube,
+  };
+};
+
+export { useCube };
